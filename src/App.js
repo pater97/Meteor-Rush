586df76b86b71254ff466404cce510b270b85e5a
pater97 <giuseppepater997@gmail.com>
2022-09-19 17:20:30 +0200

    aggiunti test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TestCanvas from './assets/screens/testcanvas/TestCanvas'
+import './assets/screens/testcanvas/testCanvas.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <TestCanvas/>
     </div>
   );
 }
---
+++ src/assets/screens/testcanvas/TestCanvas.js
@@ -0,0 +1,10 @@
+function Test(){
+    
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Test
