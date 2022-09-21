dc5d61d873194c67a4583637026775ac09972674
pater97 <giuseppepater997@gmail.com>
2022-09-21 12:09:39 +0200

    modparallax

--- src/screens/testcanvas/TestCanvas.js
+++ src/screens/testcanvas/TestCanvas.js
@@ -3,13 +3,17 @@ function Test() {
         return (
                 <div className="container">
                         {/* layer */}
-                        <div className='layer1'></div>
-                        <div className='layer2'></div>
-                        <div className="layer3"></div>
+                        <div className='layer1 baseSetting'></div>
+                        <div className='layer2 baseSetting'></div>
+                        <div className="layer3 baseSetting"></div>
+                        {/* roccie */}
+                        <div className="rock1 baseSetting"></div>
+                        <div className="rock2 baseSetting"></div>
                         {/* planet */}
-                        <div className="planet1">
-                                
-                        </div>
+                        <div className="planet1"></div>
+                        <div className="planet2"></div>
+                        <div className="planet3"></div>
+                        <div className="planet4"></div>
                 </div>
         )
 }
