96c443e8671808ec4e367aca8fbd6f7408b34b20
Oranfreezah <oranfreezah@gmail.com>
2022-09-21 12:08:57 +0200

    Added logic for jumping

--- src/components/class-components/game-box/GameBox.js
+++ src/components/class-components/game-box/GameBox.js
@@ -12,24 +12,34 @@ function GameBox() {
   const JUMP_HEIGHT = 200;
   const OBSTACLE_HEIGHT = 100;
   const OBSTACLE_WIDTH = 40;
+  const CHARACTER_LEFT = 50;
+  const JUMP_SPEED = 10;
 
-  const [charaPosition, setCharaPosition] = useState(500);
+  const [charaPosition, setCharaPosition] = useState(200);
   const [obstacleHeight, setObstacleHeight] = useState(OBSTACLE_HEIGHT);
   const [obstacleLeft, setObstacleLeft] = useState(BOX_WIDTH + OBSTACLE_WIDTH);
-  const [score, setScore] =useState(0)
-  const [collider, setCollider] = useState(false)
+  const [score, setScore] = useState(0);
+  const [collider, setCollider] = useState(false);
+  const [jump, setJump] = useState(0);
 
   useEffect(() => {
+    //FALLING DOWN
     let timeId = null;
-    if (charaPosition < BOX_HEIGHT - 100) {
+    if (charaPosition < BOX_HEIGHT - 100 + jump) { //rimuovere jump
       timeId = setInterval(() => {
         setCharaPosition((charaPosition) => charaPosition + GRAVITY);
       }, 24);
+    } else if (charaPosition > BOX_HEIGHT - 100 + jump) { // rimuovere questo
+      setCharaPosition ((charaPosition) => charaPosition - JUMP_SPEED)
     }
     return () => {
       clearInterval(timeId);
     };
   });
+
+  function handleClick() {
+    setJump((jump) => charaPosition - JUMP_HEIGHT);
+  }
 
   useEffect(() => {
     let obstacleId = null;
@@ -42,37 +52,58 @@ function GameBox() {
       };
     } else {
       setObstacleLeft(BOX_WIDTH - OBSTACLE_WIDTH);
-      setObstacleHeight(Math.floor(Math.random()*(BOX_HEIGHT-300)))
+      setObstacleHeight(Math.floor(Math.random() * (BOX_HEIGHT - 300)));
     }
-    setScore((score) => score +1)
-    console.log("Score", score)
+    setScore((score) => score + 1);
+    console.log("Score", score);
   });
 
-  function handleClick() {
-    let newCharaPosition = charaPosition - JUMP_HEIGHT;
-    if (newCharaPosition < 0) {
+  function handleLEClick() {
+    //JUMPING UP
+    let timeId = null;
+    let jumpObjective = charaPosition - JUMP_HEIGHT;
+    console.log("jumpObjective", jumpObjective);
+    if (jumpObjective < 0) {
+      console.log("i passed the roof");
       setCharaPosition(0);
-    } else {
-      setCharaPosition(newCharaPosition);
+    } else if (charaPosition >= jumpObjective) {
+      timeId = setInterval(() => {
+        setCharaPosition((charaPosition) => charaPosition - JUMP_SPEED);
+      }, 24);
+      console.log("charaPosition", charaPosition);
     }
+    return () => {
+      clearInterval(timeId);
+    };
   }
 
-  useEffect (() => {
-    const collisionChecker = 
-    charaPosition >= (BOX_HEIGHT - obstacleHeight)
-  })
+  useEffect(() => {
+    const collisionChecker = charaPosition >= BOX_HEIGHT - obstacleHeight;
+    if (
+      obstacleLeft >= 0 &&
+      obstacleLeft <= OBSTACLE_WIDTH &&
+      collisionChecker
+    ) {
+      console.log("errore");
+    }
+  }, [charaPosition, obstacleHeight, obstacleLeft]);
 
   return (
     <div
-      onMouseDown={handleClick}
+      onClick={handleClick}
       style={{
         width: BOX_WIDTH,
         height: BOX_HEIGHT,
         backgroundColor: "blue",
-        overflow:"hidden"
+        overflow: "hidden",
       }}
     >
-      <Character img={Ball} position={charaPosition} width={BALLSIZE} />
+      <Character
+        img={Ball}
+        position={charaPosition}
+        left={CHARACTER_LEFT}
+        width={BALLSIZE}
+      />
 
       <Obstacle
         img={Column}
