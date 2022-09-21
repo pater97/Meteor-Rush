e7fca3fab52c03dfdc15d7404f00482cc5bc429f
Alessandro <alessandro.dragone3@studio.unibo.it>
2022-09-21 15:24:56 +0200

    Modifiche CSS login

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Login />
-      {/* <GameBox /> */}
-      <TestCanvas />
+      {/*<GameBox />*/}
+      {/*<TestCanvas />*/}
     </div>
   );
 }
