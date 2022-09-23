23b962d93c97cd5e2e92a95cd9c34716e31bdb1b
Alessandro <alessandro.dragone3@studio.unibo.it>
2022-09-23 15:43:52 +0200

    Pulsanti GameOver

--- src/screens/gameOver/GameOver.js
+++ src/screens/gameOver/GameOver.js
@@ -1,17 +1,32 @@
 import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../components/ui/button/PersonalButton';
 import '../gameOver/gameOver.css'
 
+
 function GameOver() {
+
+    let navigate = useNavigate()
+
+    function playAgain(){
+        navigate('/')
+    }
+
+    function goTo(){
+        navigate('/ranking')
+    }
 
     return (
         <div id="gameOver">
             <div className='gameover-container'>
-                    <h2 className='gameover'>
-                        GAME OVER
-                    </h2>
+                <h2 className='gameover'>
+                    GAME OVER
+                </h2>
                 <div className='lottiAstro'>
                     <Player
-                    className='astronaut'
+                        className='astronaut'
                         autoplay
                         loop
                         src="https://assets2.lottiefiles.com/packages/lf20_if8bcea1.json"
@@ -22,6 +37,17 @@ function GameOver() {
                         />
                     </Player>
                 </div>
+
+                <Button
+                    callBack={playAgain}
+                    label={"GIOCA ANCORA"}
+                />
+
+                <Button
+                    callBack={goTo}
+                    label={"CLASSIFICA"}
+                />
+
             </div>
         </div>
     )
