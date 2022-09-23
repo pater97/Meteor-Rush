0afca2b019513035bf7a97d63141a6cab6a4c1c0
Alessandro <alessandro.dragone3@studio.unibo.it>
2022-09-23 16:43:49 +0200

    Ordine ranking

--- src/screens/ranking/Ranking.js
+++ src/screens/ranking/Ranking.js
@@ -6,12 +6,44 @@ function Ranking() {
     const [value, setValue] = useState(false)
     const [users, setUsers] = useState([])
 
-    // let users = []
+    let name = []
+    let score = []
 
     useEffect(() => {
         let users = JSON.parse(localStorage.getItem("users"))
         setUsers(users)
         setValue(true)
+
+        for (let i = 0; i < users.length; i++) {
+            score.push(users[i]["score"])
+            name.push(users[i]["name"])
+        }
+
+        for (let i = 0; i < score.length; i++) {
+            for (let j = 0; j < (score.length - i - 1); j++) {
+
+                if (score[j] > score[j + 1]) {
+
+                    let temp = score[j]
+                    score[j] = score[j + 1]
+                    score[j + 1] = temp
+                    temp = name[j]
+                    name[j] = name[j + 1]
+                    name[j + 1] = temp
+
+                }
+            }
+        }
+
+        name = name.reverse()
+        score = score.reverse()
+
+        for (let i = 0; i < users.length; i++){
+            users[i] = {
+                "name": name[i], "score": score[i]
+            }
+        }
+
     }, []);
 
     return (
